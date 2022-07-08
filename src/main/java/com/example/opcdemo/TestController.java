243b6b5861938860304dc5f9524df3213db950ad
anfernandez103 <105761103+anfernandez103@users.noreply.github.com>
2022-07-08 15:01:28 -0300

    Update TestController.java

--- src/main/java/com/example/opcdemo/TestController.java
+++ src/main/java/com/example/opcdemo/TestController.java
@@ -11,12 +11,8 @@ public class TestController {
     @Value("${forr.openshift.someproperty}")
     String someProperty;
 
-    @Value("${spring.datasource.username}")
-    String dbUser;
-
-
     @GetMapping(path = "/hello/{name}")
     public String home(@PathVariable(value = "name") String name){
-        return "Hello " + name + " with some property with db user: " + dbUser;
+        return "Hello " + name + " with some property with db user: " + name;
     }
 }
